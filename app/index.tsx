adfa19b85bd742fcf59cb524d54d16ae5d8342e9
emmilly <kaytheemmilly@gmail.com>
2025-04-29 11:33:19 -0300

    lista de usuarios (falta terminar)

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
 import { Text, View, StyleSheet } from "react-native";
 
+const usuarios = [
+  {
+    id: 1,
+    nome: "Emmilly",
+    cargo: "Integrador"
+  }, //{}objetos
+  {
+    id: 2,
+    nome: "Kaythe",
+    cargo: "Integrador"
+  }
+];
+
 export default function Index() {
   return (
     <View //div
-      style={style.container} //fontWeigth: 'bold'
+      style={style.container} //fontWeigth: 'bold' ou style.container
     >
-      <Text>olá</Text>
-      <Text>olá</Text>
-      <Text>olá</Text>
-      <Text>olá</Text>
-
+     { 
+     usuarios.map (item => ( 
+      <Text style ={{fontSize:20}}>{item.nome}</Text>
+     ))
+     }
     </View>
   );
 }
@@ -17,8 +30,9 @@ export default function Index() {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row', // faz com que os texto fiquem um do lado do outro
+    //flexDirection: 'row', // faz com que os texto fiquem um do lado do outro
     justifyContent: "center", //flex-start joga para esquerda 
     alignItems: "center",
   }}
+
 )
